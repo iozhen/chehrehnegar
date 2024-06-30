e17ce4116bcc7f39016867760f0e268fd3af19d6
mr-Hamji <mohamadrezahanji@gmail.com>
2024-06-30 15:03:53 +0330

    add two language

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,11 +1,40 @@
 import React, { ReactNode, useEffect, useState } from "react";
 import Header from "./Header";
+import English from "@/data/en.json";
+import Persian from "@/data/fa.json";
+import i18n from "i18next";
+
+import { useTranslation, initReactI18next } from "react-i18next";
 
 interface LayoutProps {
   children: ReactNode;
 }
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources: {
+      en: {
+        translation: English,
+      },
+      fa: {
+        translation: Persian,
+      },
+    },
+    lng: "en", // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    },
+  });
 
 function Layout({ children }: LayoutProps) {
+  const { t } = useTranslation();
+  // i18n.changeLanguage("fa");
+
   return (
     <div>
       <div className="bg-[#465261] min-h-[100vh] w-[100vw]">

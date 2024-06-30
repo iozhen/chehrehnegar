bb801e9d3274b9ef0cdba0d65be373a2499d04ff
Amirhomayoun Azizi <AlirezaJodat2001@gmail.com>
2024-06-30 16:46:16 +0330

    about page finished

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,19 +1,43 @@
+import { useEffect, useState } from "react";
 import React from "react";
+import data from "../../data/db.json";
+import Link from "next/link";
+
+interface linksType {
+  href: string;
+  title: string;
+}
 
 const Header = () => {
+  const [headerShowState, setHeaderShowState] = useState<boolean>();
+
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => {
+      window.scrollY > 150
+        ? setHeaderShowState(true)
+        : setHeaderShowState(false);
+    });
+  }, []);
+
   return (
-    <header className="fixed top-0 left-0 h-[10%] w-full bg-[#0007] flex justify-between items-center">
+    <header
+      style={{ background: `${headerShowState ? "#0007" : "#0000"}` }}
+      className="fixed top-0 left-0 h-[10%] w-full flex justify-between items-center"
+    >
       <a
         href="https://en.sharif.edu/"
         target="_blank"
         className="w-[76px] h-[76px] inline-block bg-[url(/images/sharif.png)] bg-cover bg-center"
       ></a>
+
       <nav className="text-white flex justify-center items-center gap-[40px] text-[1.5vw] pr-[35px]">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Team</a>
-        <a href="#">Contact Us</a>
-        <a href="#">فا</a>
+        {data.headerLinks.map(({ href, title }: linksType, index) => (
+          <Link href={href} key={index}>
+            {title}
+          </Link>
+        ))}
+
+        <button>فا</button>
       </nav>
     </header>
   );
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,33 +1,50 @@
-import React from "react";
+import data from "../data/db.json";
+
+interface aboutListType {
+  title: string;
+}
 
 const About = () => {
   return (
     <div>
-      <div>hero</div>
-      <h2>About</h2>
-      <p>
-        BINA is an AI-based WebGIS platform for environmental monitoring
-        purposes. BINA uses the state-of-the-art AI-based techniques to process
-        Sentinel/Landsat images and extract quantitative and qualitative
-        information on water and other environmental resources. The optimized
-        architecture of BINA allows efficient processing, storage, display, and
-        navigation of geospatial Earth data, with the aid of Earth Engine. BINA
-        currently monitors the history of surface area and eutrophication status
-        in 55 reservoirs across Iran. Modules on wetland monitoring,
-        agricultural water consumption, early flood forecasting and awareness,
-        and aerosol dynamics will apprear soon. BINA has been developed by the
-        computational hydrology and remote sensing research group at Sharif
-        University of Technology, with the support of the Office of Vice
-        President for Research and Technology.
-      </p>
-      <h2>Some example applications</h2>
-      <ul>
-        <li>Monitoring the area of dams and wetlands</li>
-        <li>Monitoring the eutrophication level in dams and wetlands</li>
-        <li>Monitoring the intensity and spatial extent of aerosol</li>
-        <li>Flood forecast and early warning</li>
-        <li>Monitoring agricultural water consumption</li>
-      </ul>
+      <img
+        src="/images/shadegan_header.png"
+        className="w-full block mb-[40px]"
+      />
+
+      <div className="w-[75%] mx-auto mb-[70px]">
+        <h3 className="text-[35px] font-bold">About</h3>
+
+        <p className="text-[25px] font-normal text-justify leading-[40px] mb-[30px]">
+          BINA is an AI-based WebGIS platform for environmental monitoring
+          purposes. BINA uses the state-of-the-art AI-based techniques to
+          process Sentinel/Landsat images and extract quantitative and
+          qualitative information on water and other environmental resources.
+          The optimized architecture of BINA allows efficient processing,
+          storage, display, and navigation of geospatial Earth data, with the
+          aid of Earth Engine. BINA currently monitors the history of surface
+          area and eutrophication status in 55 reservoirs across Iran. Modules
+          on wetland monitoring, agricultural water consumption, early flood
+          forecasting and awareness, and aerosol dynamics will apprear soon.
+          BINA has been developed by the computational hydrology and remote
+          sensing research group at Sharif University of Technology, with the
+          support of the Office of Vice President for Research and Technology.
+        </p>
+
+        <h3 className="text-[35px] font-bold pb-[30px]">
+          Some example applications
+        </h3>
+
+        <ul className="pl-[45px]">
+          {data.aboutUsList.map(({ title }: aboutListType, index: number) => (
+            <li key={index} className="list-disc text-[25px] font-normal">
+              {title}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <img src="/images/shadegan_footer.png" className="w-full opacity-50" />
     </div>
   );
 };

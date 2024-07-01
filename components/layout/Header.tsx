ffdcfa8298349b90bd77b6a18f75238578e51620
mr-Hamji <mohamadrezahanji@gmail.com>
2024-07-01 09:29:24 +0330

    fix menu and scroll

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,12 +1,24 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 const Header = () => {
+  const router = useRouter();
   return (
-    <header className="fixed top-0 left-0 h-[10%] w-full bg-[#0007] flex justify-between items-center">
+    <header
+      className={
+        "fixed top-0 left-0 w-full bg-[#0007] flex justify-between items-center " +
+        (router.pathname.includes("map") ? "h-[7vh]" : "h-[10%]")
+      }
+    >
       <a
         href="https://en.sharif.edu/"
         target="_blank"
-        className="w-[76px] h-[76px] inline-block bg-[url(/images/sharif.png)] bg-cover bg-center"
+        className={
+          "inline-block bg-[url(/images/sharif.png)] bg-cover bg-center " +
+          (router.pathname.includes("map")
+            ? "w-[45px] h-[45px]"
+            : "w-[76px] h-[76px]")
+        }
       ></a>
       <nav className="text-white flex justify-center items-center gap-[40px] text-[1.5vw] pr-[35px]">
         <a href="#">Home</a>

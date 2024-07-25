9c73397c74e350532554c53029f666dd9886244b
mr-Hamji <mohamadrezahanji@gmail.com>
2024-07-25 09:55:15 +0330

    fix next js config

--- next.config.mjs
+++ next.config.mjs
@@ -8,34 +8,4 @@ const nextConfig = {
    },
 };
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-   enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer({
-   webpack(config, { dev, isServer }) {
-      // Your existing webpack configuration
-      return config;
-   },
-});
-
-const WebpackDllBundlesPlugin = require("webpack-dll-bundles-plugin");
-
-module.exports = {
-   webpack(config, { dev, isServer }) {
-      if (!dev) {
-         config.plugins.push(
-            new WebpackDllBundlesPlugin({
-               filename: "vendor.js",
-               path: path.join(__dirname, "static/dll"),
-               entry: {
-                  vendor: ["react", "react-dom"],
-               },
-            })
-         );
-      }
-      return config;
-   },
-};
-
 export default nextConfig;

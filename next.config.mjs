a5cc85f6cd58a327fb13cd489d55aab7179e2c2b
mr-Hamji <mohamadrezahanji@gmail.com>
2024-07-25 09:53:59 +0330

    change the configoration of nex js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,41 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   typescript: {
+      ignoreBuildErrors: true,
+   },
+   eslint: {
+      ignoreDuringBuilds: true,
+   },
+};
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+   enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+   webpack(config, { dev, isServer }) {
+      // Your existing webpack configuration
+      return config;
+   },
+});
+
+const WebpackDllBundlesPlugin = require("webpack-dll-bundles-plugin");
+
+module.exports = {
+   webpack(config, { dev, isServer }) {
+      if (!dev) {
+         config.plugins.push(
+            new WebpackDllBundlesPlugin({
+               filename: "vendor.js",
+               path: path.join(__dirname, "static/dll"),
+               entry: {
+                  vendor: ["react", "react-dom"],
+               },
+            })
+         );
+      }
+      return config;
+   },
+};
 
 export default nextConfig;
